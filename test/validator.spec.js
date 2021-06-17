1d33a2859007cd7eb5fa3be02e21668312ef9859
Emily3000 <emily.ferpanebra@gmail.com>
2021-06-17 09:34:32 -0500

    cambios

--- test/validator.spec.js
+++ test/validator.spec.js
@@ -1,71 +1,35 @@
-const validator = {
-  // ...
-    isValid:(creditCardNumber) =>{
-    //Obtenemos el valor de la tarjeta que es string
-    let tarjeta =creditCardNumber; 
-    console.log(tarjeta);
-    //Convertimos en un array de letras
-    let reversoTarjetaArray = tarjeta.split("");
-    console.log(reversoTarjetaArray)
-    //Obtenemos el reverso del valor de la tarjeta Array
-    let reversoTarjeta=reversoTarjetaArray.reverse();
-    console.log(reversoTarjeta);
-    //El algoritmo pide las posiciones pares, pero un array inicia en 0, entonces
-    //debemos de capturar los valores de 1 3 5 6, etc..
-    
-    for(let i =0 ;i<reversoTarjeta.length;i++){
-        // Ya que buscamos las posiciones pares estas las impares del array
-        if(i%2!=0){
-            let aux =parseInt(reversoTarjeta[i])*2
-                if(aux>=10){     
-                console.log(Math.trunc(aux/10))
-                console.log(aux%10)
-                //Obtenemos la parte entera con math.trunc https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Math/trunc
-                let aux2=Math.trunc(aux/10) + aux%10;
-                reversoTarjeta[i]=aux2+"";
-            }else{
-                //el valor inicial de la posicion i se reemplaza con el valor x2
-                reversoTarjeta[i]=aux +"";
-            }
-        }else{
-            console.log("no entre")
-        }
-    }
-    console.log(reversoTarjeta)
-    let suma=0;
-    for(let i=0;i<reversoTarjeta.length;i++){
-        suma+=parseInt(reversoTarjeta[i]);
-    }
-    console.log(suma);
-    //Debemos de comprobar si es multiplo de 10
-    if(suma%10==0){
-        return true;
-    }else{
-        return false;
-    }
-},
-
-    maskify: (creditCardNumber, resultado)=>{
-            //comvertimos en array
-            console.log(creditCardNumber)
-            let tarjetaArray = creditCardNumber.split('');
-            for (let i =0; i<tarjetaArray.length-4;i++){
-                tarjetaArray[i]='*';
-            }
-            let tarjetaMaskify="";
-            for (let i =0; i<tarjetaArray.length;i++){
-                tarjetaMaskify+=tarjetaArray[i];
-            }
-            if(resultado){
-                alert("La tarjeta "+tarjetaMaskify +" es válido")
-            }else{
-                alert("La tarjeta "+tarjetaMaskify +" no es válido")
-            }
-            console.log(tarjetaArray)
-
-    }
-
-    
-};
-
-export default validator;
+// importamos el objeto `validator`, que contiene las funciones `isValid` y `maskify`
+import validator from '../src/validator';
+describe('validator', () => {
+  it('debería ser un objeto', () => {
+    expect(typeof validator).toBe('object');
+  });
+  describe('validator.isValid', () => {
+    it('debería ser una función', () => {
+      expect(typeof validator.isValid).toBe('function');
+    });
+    it.skip('debería retornar true para "4083952015263"', () => {
+      // escribe aquí tu test
+    });
+    it.skip('debería retornar true para "79927398713"', () => {
+      // escribe aquí tu test
+    });
+    it.skip('debería retornar false para "1234567890"', () => {
+      // escribe aquí tu test
+    });
+  });
+  describe('validator.maskify', () => {
+    it('debería ser una función', () => {
+      expect(typeof validator.maskify).toBe('function');
+    });
+    it.skip('Debería retornar "############5616" para "4556364607935616"', () => {
+      // escribe aquí tu test
+    });
+    it.skip('Debería retornar "1" para "1"', () => {
+      // escribe aquí tu test
+    });
+    it.skip('Debería retornar "######orld" para "helloworld"', () => {
+      // escribe aquí tu test
+    });
+  });
+});
